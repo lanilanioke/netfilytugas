5763e852f09226e36ae80945f52a9fcc4c4940d4
lanilanioke <lanilanitugas@gmail.com>
2023-06-18 15:28:09 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "LANI" // TODO: replace this
+const studentId = "FE4235458" // TODO: replace this
+
+export { url, studentName, studentId }
